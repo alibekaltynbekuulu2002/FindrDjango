e865b007a44cddc2c9f5ac594008d7b611e0ba40
Alibek Altynbek uulu <alibekprogramming@gmail.com>
2023-03-07 03:04:42 +0600

    changed user views, models

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path('/',)
+    path('/', views.get_users, name='get_users'),
+    path('/<int:id>', views.get_user, name='get_user'),
+    path('/update/<int:id>', views.update_user, name='update_user'),
+    path('/delete/<int:id>', views.delete_user, name='delete_user')
 ]
--- users/views.py
+++ users/views.py
@@ -4,17 +4,21 @@ from django.http import Http404
 from rest_framework import status
 
 
+@api_view(['GET'])
 def get_users(request, *args, **kwargs):
     pass
 
 
+@api_view(['GET', 'POST'])
 def get_user(request, id):
     pass
 
 
+@api_view(['DELETE'])
 def delete_user(request, id):
     pass
 
 
+@api_view('PUT', 'PATCH')
 def update_user(request, id):
     pass
