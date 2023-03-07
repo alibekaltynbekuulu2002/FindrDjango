5201272350fc9e5d899142c7b5df670fcb3540a3
Alibek Altynbek uulu <alibekprogramming@gmail.com>
2023-03-08 04:05:20 +0600

    modified user model with nice ui admin panel

--- users/views.py
+++ users/views.py
@@ -1,24 +1,3 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from django.http import Http404
-from rest_framework import status
+from django.shortcuts import render
 
-
-@api_view(['GET'])
-def get_users(request, *args, **kwargs):
-    pass
-
-
-@api_view(['GET', 'POST'])
-def get_user(request, id):
-    pass
-
-
-@api_view(['DELETE'])
-def delete_user(request, id):
-    pass
-
-
-@api_view('PUT', 'PATCH')
-def update_user(request, id):
-    pass
+# Create your views here.
